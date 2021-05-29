fa0e17f81128cf5318cf0456f92e6988dcf4e704
유성희 <YooSeongHee@yuseong-yeong-ui-iMac.local>
2021-05-29 21:35:22 +0900

    front_01 : add React template

---
+++ src/front/src/test_app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import GlobalStyle from './globalStyles'; 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.js';
+import SignUpUser from './components/SignUp/SignUpUser.js';
+import LoginUser from './components/Login/LoginUser.js';
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle/>
+      <Navbar />
+      <Switch>
+        <Route path ='/login' component={LoginUser}/>
+        <Route path = '/signup' component ={SignUpUser}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
