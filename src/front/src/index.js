9e163b063ffbe92ae26ccf509325238fdca31e88
harvey.dent <harvey.dent@kakaocorp.com>
2021-06-20 16:43:10 +0900

    FIX: error 해결

--- src/front/src/index.js
+++ src/front/src/index.js
@@ -23,7 +23,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/scss/paper-dashboard.scss?v=1.3.0";
 import "./assets/demo/demo.css";
-import "./perfect-scrollbar/css/perfect-scrollbar.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import AdminLayout from "layouts/Admin.js";
 
