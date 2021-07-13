28ad63370a8fbeeb3f4b53e8a6e75b904e8a6563
harvey.dent <harvey.dent@kakaocorp.com>
2021-07-13 16:04:01 +0900

    fix

--- src/front/src/views/Login.js
+++ src/front/src/views/Login.js
@@ -16,152 +16,151 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import ReactDOM from "react-dom";
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Map from "../views/Map";
 // reactstrap components
 import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  FormGroup,
-  Form,
-  Input,
-  Row,
-  Col,
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    CardTitle,
+    FormGroup,
+    Form,
+    Input,
+    Row,
+    Col,
 } from "reactstrap";
 
 function Login() {
-  const[ email, setEmail] = useState('');
-  const[ password, setPwd] = useState('');
-  const [userRole, setRole] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPwd] = useState('');
+    const [userRole, setRole] = useState(false);
 
-  const handleEmailChange = (event) =>{
-      setEmail(event.target.value);
-  }
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
 
-  const handlePWDChange = (event)=>{
-      setPwd(event.target.value);
-  }
-  const handleSignUp = (event)=>{
-    //회원가입 버튼 
-  }
-  const handleLogin = (event)=>{
-      if (email === "" || password === "") {
-          alert("아이디 또는 비밀번호를 입력해주세요.");
-          return;
-      }
+    const handlePWDChange = (event) => {
+        setPwd(event.target.value);
+    }
+    const handleSignUp = (event) => {
+        //회원가입 버튼
+    }
+    const handleLogin = (event) => {
+        if (email === "" || password === "") {
+            alert("아이디 또는 비밀번호를 입력해주세요.");
+            return;
+        }
 
-      const data = {
-          body: JSON.stringify({"email" : email, "password": password}),
-          headers: {"Content-Type": "application/json"},
-          method: 'post',
-       
-      }
-      fetch("/login", data)
-          .then(res => {
-              if(!res.ok) {
-                  throw new Error(res.status);
-              } else {
-            
-                res.text().then((value)=>{
+        const data = {
+            body: JSON.stringify({"email": email, "password": password}),
+            headers: {"Content-Type": "application/json"},
+            method: 'post',
 
-                  if(value === "true"){
-                    setRole(true);
-                    console.log("Login Sucess.");
-                    ReactDOM.render(
-                      <Map />,
-                      document.getElementById('user')
-                    );
-                  }
-                  else{
-                    alert("로그인에 실패했습니다.");
-                  }
-                })
-              }
-          })
-          .catch(err => console.log(err));
-          
-  }
+        }
+        fetch("/login", data)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(res.status);
+                } else {
 
-    return(
-      <>
-      <div className="content">
-        <Row>
-          <Col md="4">
-            <Card className="card-user">
-              <CardHeader>
-                <CardTitle tag="h5">Login</CardTitle>
-                <p className="card-category">Hello!</p>
-              </CardHeader>
-              <div className="image">
-                <img
-                  alt="..."
-                  src={require("assets/img/damir-bosnjak.jpg").default}
-                />
-              </div>
-              <CardBody>
-                <div className="author">
-                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                    <img
-                      alt="..."
-                      className="avatar border-gray"
-                      src={require("assets/img/default-avatar.png").default}
-                    />
-                  </a>
-                </div>
+                    res.text().then((value) => {
+
+                        if (value === "true") {
+                            setRole(true);
+                            console.log("Login Sucess.");
+                            ReactDOM.render(
+                                <Map/>,
+                                document.getElementById('user')
+                            );
+                        } else {
+                            alert("로그인에 실패했습니다.");
+                        }
+                    })
+                }
+            })
+            .catch(err => console.log(err));
+
+    }
+
+    return (
+        <>
+            <div className="content">
                 <Row>
-                  <Col className="px-1" md="10">
-                      <FormGroup>
-                        <label>E-mail</label>
-                        <Input
-                          type="email"
-                          onChange={handleEmailChange}
-                        />
-                        </FormGroup>
-                  </Col>
-                  <Col className="px-1" md="10">
-                      <FormGroup>
-                        <label>Password</label>
-                        <Input
-                          type="password"
-                          onChange = {handlePWDChange}
-                        />
-                        </FormGroup>
-                  </Col>
-                  </Row>
-                    
-              </CardBody>
-              <CardFooter>
-                
-              <div className="update ml-auto mr-auto">
-                      <Button
-                        className="btn-round"
-                        color="primary"
-                        type="submit"
-                        onClick = {handleLogin}
-                      >
-                        Login
-                      </Button>
-                      <Button
-                        className="btn-round"
-                        color="primary"
-                        type="submit"
-                        onClick = {handleSignUp}
-                      >
-                        Sign Up
-                      </Button>
-              </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </>
+                    <Col md="4">
+                        <Card className="card-user">
+                            <CardHeader>
+                                <CardTitle tag="h5">Login</CardTitle>
+                                <p className="card-category">Hello!</p>
+                            </CardHeader>
+                            <div className="image">
+                                <img
+                                    alt="..."
+                                    src={require("assets/img/damir-bosnjak.jpg").default}
+                                />
+                            </div>
+                            <CardBody>
+                                <div className="author">
+                                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                                        <img
+                                            alt="..."
+                                            className="avatar border-gray"
+                                            src={require("assets/img/default-avatar.png").default}
+                                        />
+                                    </a>
+                                </div>
+                                <Row>
+                                    <Col className="px-1" md="10">
+                                        <FormGroup>
+                                            <label>E-mail</label>
+                                            <Input
+                                                type="email"
+                                                onChange={handleEmailChange}
+                                            />
+                                        </FormGroup>
+                                    </Col>
+                                    <Col className="px-1" md="10">
+                                        <FormGroup>
+                                            <label>Password</label>
+                                            <Input
+                                                type="password"
+                                                onChange={handlePWDChange}
+                                            />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+
+                            </CardBody>
+                            <CardFooter>
+
+                                <div className="update ml-auto mr-auto">
+                                    <Button
+                                        className="btn-round"
+                                        color="primary"
+                                        type="submit"
+                                        onClick={handleLogin}
+                                    >
+                                        Login
+                                    </Button>
+                                    <Button
+                                        className="btn-round"
+                                        color="primary"
+                                        type="submit"
+                                        onClick={handleSignUp}
+                                    >
+                                        Sign Up
+                                    </Button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        </>
     );
 }
 
--- src/front/src/views/SignUp.js
+++ src/front/src/views/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Row,
     Col,
@@ -13,39 +13,89 @@ import {
     CardTitle
 
 } from "reactstrap";
+import ReactDOM from "react-dom";
+import Map from "./Map";
 
 function SignUp() {
+    const [id, setId] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+
+    const handleIdChange = (e) => setId(e.target.value);
+    const handlePasswordChange = (e) => setPassword(e.target.value)
+    const handleNameChange = (e) => setName(e.target.value)
+    const handleEmailChange = (e) => setEmail(e.target.value);
+
+    const handleSignUp = (event) => {
+        if (email && password && name && email) {
+            alert("모든 값을 입력해주세요.");
+            return;
+        }
+
+        const data = {
+            body: JSON.stringify({"id": id, "password": password, "email": email, "name":name}),
+            headers: {"Content-Type": "application/json"},
+            method: 'post',
+
+        }/*
+        fetch("/signup", data)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(res.status);
+                } else {
+
+                    res.text().then((value) => {
+
+                        if (value === "true") {
+                            console.log("Login Sucess.");
+                            ReactDOM.render(
+                                <Map/>,
+                                document.getElementById('user')
+                            );
+                        } else {
+                            alert("로그인에 실패했습니다.");
+                        }
+                    })
+                }
+            })
+            .catch(err => console.log(err));
+*/
+    }
+
     return (
-        <> < div className = "content" > <Row>
-            <Col md="8">
-                <Card className="card-user">
-                    <CardHeader>
-                        <CardTitle tag="h5">회원가입</CardTitle>
-                    </CardHeader>
-                    <CardBody>
-                        <Form>
-                            <FormGroup>
-                                <Label for="name">이름</Label>
-                                <Input type="text" id="name"></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="id">아이디</Label>
-                                <Input type="text" id="id"></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="pw">비밀번호</Label>
-                                <Input type="password" id="pw"></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="phoneNumber">핸드폰번호</Label>
-                                <Input type="text" id="phoneNumber"></Input>
-                            </FormGroup>
-                            <Button color="primary" type="submit" onClick="">가입하기</Button>
-                        </Form>
-                    </CardBody>
-                </Card>
-            </Col>
-        </Row> </div> </>
+        <>
+            < div className="content"><Row>
+                <Col md="8">
+                    <Card className="card-user">
+                        <CardHeader>
+                            <CardTitle tag="h5">회원가입</CardTitle>
+                        </CardHeader>
+                        <CardBody>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="name">이름</Label>
+                                    <Input type="text" id="name"></Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="id">아이디</Label>
+                                    <Input type="text" id="id"></Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="pw">비밀번호</Label>
+                                    <Input type="password" id="pw"></Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="phoneNumber">핸드폰번호</Label>
+                                    <Input type="text" id="phoneNumber"></Input>
+                                </FormGroup>
+                                <Button color="primary" type="submit" onClick={handleSignUp}>가입하기</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row></div>
+        </>
     );
 }
 
